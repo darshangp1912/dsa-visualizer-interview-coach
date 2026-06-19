98acc32a769b646b534db27a8770b74ed77372df
darshangp1912 <darshangowda1912@gmail.com>
2026-06-20 00:37:39 +0530

    Add backend/src/main/java/com/dsa/visualizer/repository/QuizQuestionRepository.java

---
+++ backend/src/main/java/com/dsa/visualizer/repository/QuizQuestionRepository.java
@@ -0,0 +1,9 @@
+package com.dsa.visualizer.repository;
+
+import com.dsa.visualizer.entity.QuizQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long> {
+}
