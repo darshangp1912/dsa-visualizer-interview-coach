314c1eeb959c7f8b4a589219644b8a9fb47532eb
darshangp1912 <darshangowda1912@gmail.com>
2026-06-20 00:37:07 +0530

    Add backend/src/main/java/com/dsa/visualizer/dto/SignupRequest.java

---
+++ backend/src/main/java/com/dsa/visualizer/dto/SignupRequest.java
@@ -0,0 +1,23 @@
+package com.dsa.visualizer.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
