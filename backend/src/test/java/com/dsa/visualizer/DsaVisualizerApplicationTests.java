5289813a7c3e04e32134f141d5fab07ec9fe4fba
darshangp1912 <darshangowda1912@gmail.com>
2026-06-20 00:38:05 +0530

    Add backend/src/test/java/com/dsa/visualizer/DsaVisualizerApplicationTests.java

---
+++ backend/src/test/java/com/dsa/visualizer/DsaVisualizerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dsa.visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class DsaVisualizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Simple context loading check
+    }
+}
